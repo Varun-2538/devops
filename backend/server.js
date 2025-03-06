8c68618e1e971444305525a5a7d100abc7d44730
Varun-2538 <942525@gmail.com>
2025-03-06 09:27:11 +0530

    server update and cd pipeline

--- backend/server.js
+++ backend/server.js
@@ -100,9 +100,10 @@ app.post("/failure", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(5000, '0.0.0.0', () => {
+  console.log("Server running on http://0.0.0.0:5000");
 });
+
 
 
 //hello
